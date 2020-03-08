e858d641ea750cf7e5322368cd0cf07cfddae19f
Amit Chawla <amitchawla59@gmail.com>
2020-03-08 17:52:34 -0400

    added EPA for 2D

--- Assets/Scripts/PrismManager.cs
+++ Assets/Scripts/PrismManager.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Random = UnityEngine.Random;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class PrismManager : MonoBehaviour
 {
-    public int prismCount = 10;
+    public int prismCount = 0;
     public float prismRegionRadiusXZ = 5;
     public float prismRegionRadiusY = 5;
     public float maxPrismScaleXZ = 5;
@@ -28,7 +34,7 @@ public class PrismManager : MonoBehaviour
 
     void Start()
     {
-        Random.InitState(10);    //10 for no collision
+        Random.InitState(4);    //10 for no collision
 
         prismParent = GameObject.Find("Prisms");
         for (int i = 0; i < prismCount; i++)
@@ -154,12 +160,12 @@ public class PrismManager : MonoBehaviour
             float d1z = bz1 - az2;
             float d2z = az1 - bz2;
             //Debug.Log("az1" + az1 + "bz1" + bz1 + "az2" + az2 + "bz2");
-            if (d1z < 0 || d2z < 0)
+            if (d1z < 0 && d2z < 0)
             {
                 outputListY.Add(olx1);
             }
         }
-        //Debug.Log(outputListY.Count);
+        // Debug.Log(outputListY.Count);
         return outputListY;
         /*   
                var temp = new List<Prism>();
@@ -247,26 +253,40 @@ public class PrismManager : MonoBehaviour
         return outputListX;
 
     }
-
-
-
-
+    
     private bool CheckCollision(PrismCollision collision)
     {
         var prismA = collision.a;
         var prismB = collision.b;
         
-        collision.penetrationDepthVectorAB = Vector3.zero;
-        
-        var minkowski = new List<Vector3>();
+        List<Vector3> minkowski = new List<Vector3>();
         foreach(var p in prismA.points){
             foreach(var p2 in prismB.points){
                 minkowski.Add( p - p2);
             }
         }
-
         
+        // draw the minkowski
+        // for (var i = 0; i < minkowski.Count(); i++)
+        // {
+        //     Debug.DrawLine(minkowski[i], minkowski[i] + new Vector3(0.0f, 0f, 0.05f), Color.magenta, UPDATE_RATE);
+        // }
 
+        bool colliding = GJK(prismA, prismB); 
+        if (colliding)
+        {   
+            collision.penetrationDepthVectorAB = EPA(prismA, prismB);
+        }
+        else
+        {
+            collision.penetrationDepthVectorAB = Vector3.zero;
+        }
+
+        return colliding;
+    }
+
+    private bool GJK(Prism prismA, Prism prismB)
+    {
         pointList = new List<Vector3>();
         // Start at a random point
         dir = new Vector3(1,0,1);
@@ -290,6 +310,87 @@ public class PrismManager : MonoBehaviour
         return false;
     }
 
+    private Vector3 EPA(Prism prismA, Prism prismB)
+    {
+        // 1. Find the side of simplex closet to the origin
+        var n = pointList.Count();
+        var minDist = float.MaxValue;
+        var prevDist = 0f;
+        Tuple<Vector3, Vector3> closestSide = null;
+        Vector3 support = Vector3.zero;
+        var normal = Vector3.zero;
+                
+        List<Tuple<Vector3, Vector3>> simplex = new List<Tuple<Vector3, Vector3>>();
+
+
+        for (var i = 0; i < n; i++)
+        {
+            simplex.Add(new Tuple<Vector3, Vector3>(pointList[i], pointList[(i + 1) % n]));
+        }
+
+        while (Mathf.Abs(prevDist - minDist) > 0.01f)
+        {
+            // DrawSimplex(simplex);
+            prevDist = minDist;
+
+            // List<Ray> toAdd = new List<Ray>();
+            foreach (var side in simplex)
+            {
+                float distanceFromOrigin = Vector3.Cross(side.Item2 - side.Item1, -side.Item1).magnitude;
+                if (distanceFromOrigin < minDist)
+                {
+                    minDist = distanceFromOrigin;
+                    closestSide = side;
+                }
+            }
+
+            // find normal to this vector in the outward direction (away from origin)
+            var direction = closestSide.Item2 - closestSide.Item1;
+            normal = new Vector3(-direction.z, 0f, direction.x);
+            if (Vector3.Dot(normal, -closestSide.Item1) > 0)
+            {
+                normal = -normal;
+            }
+
+            // find support point on minkowski in the direction of this normal vector
+            support = getSupport(prismA, prismB, normal);
+            
+            // remove this side from from the list and add the two new sides in the list
+            simplex.Remove(closestSide);
+            simplex.Add(new Tuple<Vector3, Vector3>(closestSide.Item1, support - closestSide.Item1));
+            simplex.Add(new Tuple<Vector3, Vector3>(support, closestSide.Item2));
+            
+        }
+        // Debug.DrawLine(Vector3.zero, support, Color.yellow,UPDATE_RATE);
+        return support * 1.05f;
+    }
+
+    private void DrawSimplex(List<Tuple<Vector3, Vector3>> simplex)
+    {
+        foreach (var side in simplex)
+        {
+            Debug.DrawLine(side.Item1, side.Item2, Color.blue, UPDATE_RATE);
+        }
+    }
+
+    private Vector3 Support(List<Vector3> points, Vector3 dir)
+    {
+        var maxProjection = float.MinValue;
+        Vector3 support = Vector3.zero;
+        ;
+        foreach (var p in points)
+        {
+            var dist = Vector3.Dot(p, dir);
+            if (dist > maxProjection)
+            {
+                maxProjection = dist;
+                support = p;
+            }
+        }
+
+        return support;
+    }
+    
     private bool OinSimplex(Prism prismA,Prism prismB) {
         if(pointList.Count == 2) {
             if (Vector3.Dot(pointList[0]-pointList[1],-pointList[1]) > 0){
@@ -377,8 +478,14 @@ public class PrismManager : MonoBehaviour
         var pushA = -collision.penetrationDepthVectorAB / 2;
         var pushB = collision.penetrationDepthVectorAB / 2;
 
-        prismObjA.transform.position += pushA;
-        prismObjB.transform.position += pushB;
+        for (int i = 0; i < collision.a.pointCount; i++)
+        {
+            collision.a.points[i] += pushA;
+        }
+        for (int i = 0; i < collision.b.pointCount; i++)
+        {
+            collision.b.points[i] += pushB;
+        }
 
         Debug.DrawLine(prismObjA.transform.position, prismObjA.transform.position + collision.penetrationDepthVectorAB, Color.cyan, UPDATE_RATE);
     }
